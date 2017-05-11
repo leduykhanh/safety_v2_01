0f46cf480fab90d00c741100e09c85b038d8bc08
jangkoo <le.khanh@goldenequator.com>
2017-05-11 17:36:10 +0800

    config

--- config/footer.php
+++ config/footer.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<div class="alert alert-info">
-    <strong>tutorial link !</strong> <a href="http://cleartuts.blogspot.com/2015/04/php-pdo-crud-tutorial-using-oop-with.html">cleartuts</a>!
+    <strong>Powered by </strong> <a href="http://jangkoo.com">Jangkoo</a>!
 	</div>
 </div>
 <script src="bootstrap/js/bootstrap.min.js"></script>
